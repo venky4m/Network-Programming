bd68029b3c1a4158b5e0a7ce74c97b5ab33a32f5
Venkatesh Marripelly <74142589+venky4m@users.noreply.github.com>
2022-12-16 13:10:24 +0530

    Update server.c

--- Server - client/server.c
+++ Server - client/server.c
@@ -1,3 +1,9 @@
+/**
+ * Author : Venkatesh Marripelly
+ * File : server.c
+ * 
+*/
+
 #include<stdio.h>
 #include<sys/types.h>
 #include<sys/socket.h>
@@ -6,69 +12,100 @@
 #include<string.h>
 #include<unistd.h>
 #include <arpa/inet.h>
-#define PORT 13331
-
+#define PORT 12221
 
 void print_address(struct sockaddr_in addr)
 {
     printf("{\n\t");
-    printf(".sin_family :%d\n",addr.sin_family);
-    printf("\t.sin_port :%d\n",addr.sin_port);
-    printf("\t.sin_addr.s_addr :%d\n",addr.sin_addr.s_addr);
+    printf("addr.sin_family :%d\n",addr.sin_family);
+    printf("\taddr.sin_port :%d\n",addr.sin_port);
+    printf("\taddr.sin_addr.s_addr :%d\n",addr.sin_addr.s_addr);
     printf("}\n");
 }
-int main()
-{
+
+int main(int argc,char *argv[])
+{   
     int main_socket_fd;
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     int server_addr_int = 0;
-    socklen_t len_of_server_addr = sizeof(server_addr);
+    socklen_t len_of_sever_addr = sizeof(server_addr);
     socklen_t len_of_client_addr = 0;
-    if((main_socket_fd = socket(AF_INET,SOCK_STREAM,0)) < 0 )
-    {
-        printf("socket error..\n");
-        exit(0);
-    }
-    server_addr.sin_port = htons(12221);
-    server_addr.sin_family = AF_INET;
-    inet_pton(AF_INET,"127.0.0.1",&server_addr_int);
-    server_addr.sin_addr.s_addr = server_addr_int;
-    print_address(server_addr);
 
-    int con_fd = 0;
-    con_fd = connect(main_socket_fd,(struct sockaddr *)&server_addr,len_of_server_addr);
-    
-        printf("%d",con_fd);
+    main_socket_fd = socket(AF_INET,SOCK_STREAM,0);
+        
+        //prepare the address of server
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET,"192.168.0.108",&server_addr_int);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+        
+    print_address(server_addr);
+        
+    //bind --> this function is to binding our main_socket to function on
+    //specified server_addr
+    if(bind(main_socket_fd,(struct sockaddr *)&server_addr,len_of_sever_addr) == -1)
+    {
+        perror("bind error\n");
+        exit(1);
+    }
+
+    //the purpose of listen call is just to set the queue length
+    //queue length is number of max of connection that can be placed in queue
+    //so it is a non blocking system call
+
+    if(listen(main_socket_fd,5) == -1)
+    {
+        perror("listen error\n");
+        exit(1);
+    }
+    //connection came after this listen() are all be placed in queue
+    //before listen() , any connection request will be dropped
+    printf("listen done..\n");
+    //accept() call take the one connection request from queue and give the address of that client
+    //if no connection in already in queue ,then accept method block untill a new connection come
+    //returns a new file descriptor referring to that socket
+    /*
+         int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+        */
+    int accept_fd = 0;
+    while((accept_fd = accept(main_socket_fd,(struct sockaddr *)&client_addr,(socklen_t *)&len_of_client_addr))!=-1)
+    {
+        printf("A client connected with -> accept_fd = %d",accept_fd);
         print_address(client_addr);
-        printf("\ncommunication started with server..\n\n");
+        printf("\ncommunication started with client..\n\n");
         char buffer_in[512];
         char buffer_out[512];
         memset(buffer_in,0,sizeof(buffer_in));
         memset(buffer_out,0,sizeof(buffer_out));
+        
         while( 1 )
         {
-            printf("client : ");
-            int len = 0;
-            memset(buffer_in,0,sizeof(buffer_in));
-            memset(buffer_out,0,sizeof(buffer_out));
-            fflush(stdin);
-            scanf("%[^\n]",buffer_out);
-            if((len = send(main_socket_fd,buffer_out,512,0))<0)
+            //memcpy(buffer_out,"communicating");
+            int len = read(accept_fd,buffer_in,512);
+            if(len<=0)
             {
-                printf("error while sending...\n");
-                exit(1);
+                printf("recv error\n..");
+                exit(0);
             }
-            if((len = read(main_socket_fd,buffer_in,512))<0)
-            {
-                printf("error while reading..\n");
-                exit(1);
-            }
-            if(strcmp(buffer_out,"exit") == 0)
+            if(strcmp(buffer_in,"exit") == 0)
             {
                 printf("Bye Bye");
                 exit(0);
             }
-            printf("server : %s\n",buffer_in);
+            printf("client : %s\n",buffer_in);
+            printf("server : ");
+            memset(buffer_in,0,sizeof(buffer_in));
+            memset(buffer_out,0,sizeof(buffer_out));
+            fflush(stdin);
+            scanf("%[^\n]",buffer_out);
+            //printf("%s",buffer_out);
+            if((len = send(accept_fd,buffer_out,512,0))<0)
+            {
+                printf("seding error\n");
+                exit(1);
+            }           
         }
+    }
+    return 0;
 }
